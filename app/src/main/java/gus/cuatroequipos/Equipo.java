67303f1258f244151a15211ed425c340476ba78a
nibit1990 <allende53@gmail.com>
2016-11-28 21:42:18 -0300

    Initial commit

---
+++ app/src/main/java/gus/cuatroequipos/Equipo.java
@@ -0,0 +1,36 @@
+package gus.cuatroequipos;
+
+/**
+ * Created by Gus on 26/09/2016.
+ */
+public class Equipo {
+
+    //Atributos
+    int id;
+    String equipo ;
+    int puntos ;
+
+    // Constructores
+
+    public Equipo(int id, String equipo ) {
+        this.id = id;
+        this.equipo = equipo;
+
+    }
+
+
+
+    //Comportamientos - metodos
+
+    public void mostrarEquipos(){
+        // Comportamiento del metodo
+        System.out.println("Datos del telefono");
+        System.out.println("ID:" + id);
+        System.out.println("Equipo: " + equipo);
+        System.out.println("Puntos: " + puntos);
+
+    }
+
+
+
+}
